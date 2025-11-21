20087eba339d63e8ccc9c7d8d78fff08dacb167d
carlosgpintoo <carlosg.pintoo@udea.edu.co>
2025-11-20 23:09:30 -0500

    creamos costos.py
    este es el codigo encargado de tomar la orden, ver de cuanto necesitamos cada cosa y cuanto es el total para el cliente

---
+++ src/costos.py
@@ -0,0 +1,40 @@
+precios = {
+    'habitacion_individual': 7,
+    'habitacion_pareja': 10,
+    'habitacion_familia': 24,
+    'comida': 2,
+    'paseo': 10,
+    'mascota': 8
+}
+
+def calcular_costos(tipo_habitacion, comida, paseo, cantidad_mascotas):
+    
+    if tipo_habitacion == "individual":
+        costo_habitacion = precios['habitacion_individual']
+    elif tipo_habitacion == "pareja":
+        costo_habitacion = precios['habitacion_pareja']
+    elif tipo_habitacion == "familia":
+        costo_habitacion = precios['habitacion_familia']
+    else:
+        print("Tipo de habitación no válido.")
+        return
+
+    
+    costo_comida = comida * precios['comida']
+    costo_paseo = paseo * precios['paseo']
+    costo_mascotas = cantidad_mascotas * precios['mascota']
+    
+    total = costo_habitacion + costo_comida + costo_paseo + costo_mascotas
+    
+    
+    print(f"Costo total de la reserva: ${total}")
+    print(f"Resuemn de la reserva: \n- Habitación: ${costo_habitacion} \n- Comidas: ${costo_comida} \n- Paseos: ${costo_paseo} \n- Mascotas: ${costo_mascotas}")
+
+
+tipo_habitacion = input("Ingrese el tipo de habitación (individual, pareja, familia): ").lower()
+comida = int(input("¿Cuántas comidas adicionales? "))
+paseo = int(input("¿Cuántos paseos adicionales? "))
+cantidad_mascotas = int(input("¿Cuántas mascotas? "))
+
+
+calcular_costos(tipo_habitacion, comida, paseo, cantidad_mascotas)
